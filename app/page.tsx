8f71b0d08509d10876c97b83142b16226589b77f
Nicholas <nicholas.cruickshank25@gmail.com>
2023-05-08 21:47:23 +1000

    responsive nav bar added to save the pain later

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-semibold text-2xl">
+    <main className="flex flex-col items-center justify-between p-24 font-semibold text-2xl">
       <h1>Main Page</h1>
     </main>
   );

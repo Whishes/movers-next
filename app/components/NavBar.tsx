ec108df6ca5c5bd12eada735f1e15fdf2259bc85
Nicholas <nicholas.cruickshank25@gmail.com>
2023-05-08 17:59:19 +1000

    basic nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="flex h-16 justify-between items-center">
+      <h2 className="text-3xl">Movers</h2>
+      <nav className="mr-5 flex items-center justify-center gap-4">
+        {/* try adding hover animation to the border bottom */}
+        <Link
+          className="grow-bar grow-bar-animation p-2 after:bg-blue-500"
+          href="/"
+        >
+          Home
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;

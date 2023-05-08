78bd32010da3812f8df510ee722d4dafba953516
Nicholas <nicholas.cruickshank25@gmail.com>
2023-05-08 17:43:47 +1000

    quick route test, layout changes, etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
+import NavBar from "./components/NavBar";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  fallback: ["system-ui", "arial"],
+});
 
 export const metadata = {
   title: "Movers",
@@ -15,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Main Page</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-semibold text-2xl">
+      <h1>Main Page</h1>
     </main>
   );
 }
